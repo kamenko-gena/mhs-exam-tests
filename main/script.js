b9d5704edb5762ca0dc3356f85c31a05a04d13c6
Kamenko Gena <kamenko.gennady@gmail.com>
2024-08-15 07:42:45 +0300

    Delete padding in question block

--- main/script.js
+++ main/script.js
@@ -3698,7 +3698,7 @@ function buildQuiz() {
   answers.sort(() => Math.random() - 0.5);
 
   output.push(
-    `<div class="question" style="padding: 0 5em; font-size: large; font-weight: 600; text-align: center"> ${currentQuestionData.question} </div>`
+    `<div class="question" style="font-size: large; font-weight: 600; text-align: center"> ${currentQuestionData.question} </div>`
   );
 
   if (currentQuestionData.image) {
