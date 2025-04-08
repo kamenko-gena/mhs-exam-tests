7dbae49f9b46761c113af901bbbba02d87b2418c
unknown <kamenko.gennady@gmail.com>
2025-04-08 11:57:16 +0300

    rotes: turn off guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,14 @@ import { MhsMontazhQuestionsComponent } from './components/mhs-montazh-questions
 import { MhsToQuestionsComponent } from './components/mhs-to-questions/mhs-to-questions.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
-import { authenticationGuard } from './guard/authentication.guard';
 
 export const appRoutes: Route[] = [
     {
         path: 'mhs-montazh',
-        canActivate: [authenticationGuard],
         component: MhsMontazhQuestionsComponent,
     },
     {
         path: 'mhs-to',
-        canActivate: [authenticationGuard],
         component: MhsToQuestionsComponent,
     },
     {
